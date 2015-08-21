b4b8f7b1c853af6b27aa82f16c7ab23266ad6d03
Sophia Elizabeth Shapira <Sophia.Shapira@gmail.com>
2015-08-21 02:01:55 -0400

    Finally, I have enough to run my Routine Manager

--- inner-wrap.pl
+++ inner-wrap.pl
@@ -2,3 +2,22 @@ use strict;
 use plelorec;
 use argola;
 use alarmica;
+
+sub opto__f_run {
+  my @lc_a;
+  @lc_a = &argola::remrg;
+  &plelorec::flplex(@lc_a);
+} &argola::setopt("-run",\&opto__f_run);
+
+sub opto__f_set {
+  my $lc_a;
+  $lc_a = &argola::getrg;
+  &alarmica::regmsg($lc_a);
+} &argola::setopt("-set",\&opto__f_set);
+
+sub opto__f_entr {
+}
+
+&argola::runopts;
+
+
