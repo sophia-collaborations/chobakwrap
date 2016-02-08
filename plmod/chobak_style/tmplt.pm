d3e278b8b1b1cf7949d87dd5c15d9c706b9a7f60
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-02-08 11:41:20 -0500

    Begin groundwork for conditionals in style-templates

--- plmod/chobak_style/tmplt.pm
+++ plmod/chobak_style/tmplt.pm
@@ -9,18 +9,29 @@ sub new {
   my $lc_cmd;
   my $lc_cont;
   my $lc_rcont;
-  my $lc_flare;
-  my $lc_aflare;
-  my $lc_incm;
-  my $lc_known;
-  my @lc_parts;
-  my @lc_algo;
   
   $this = chobak_style::tmplt::cls->__raw_new;
   $lc_cmd = "cat";
   &wraprg::lst($lc_cmd,$_[0]);
   $lc_cont = `$lc_cmd`;
   $lc_rcont = $lc_cont;
+  $this->{'alg'} = &procpart($lc_rcont,$_[0]);
+  return $this;
+}
+
+sub procpart {
+  my $lc_rcont;
+  my @lc_algo;
+  my @lc_parts;
+  my $lc_known;
+  my $lc_flare;
+  my $lc_incm;
+  my $lc_aflare;
+  my $lc_returno;
+  my $lc_filename;
+  
+  $lc_rcont = $_[0];
+  $lc_filename = $_[1];
   
   @lc_algo = ();
   while ( $lc_rcont ne "" )
@@ -42,23 +53,44 @@ sub new {
       @lc_algo = (@lc_algo,[\&chobak_style::tmplt::util::var_esc,[$lc_parts[1]]]);
       $lc_known = 10;
     }
+    if ( $lc_parts[0] eq 'ifyes' )
+    {
+      @lc_algo = (@lc_algo,[\&chobak_style::tmplt::util::if_yes,[$lc_parts[1]
+        ,&procpart($lc_rcont,$lc_filename)]])
+      ;
+      $lc_known = 10;
+    }
+    if ( $lc_parts[0] eq 'ifno' )
+    {
+      @lc_algo = (@lc_algo,[\&chobak_style::tmplt::util::if_no,[$lc_parts[1]
+        ,&procpart($lc_rcont,$lc_filename)]])
+      ;
+      $lc_known = 10;
+    }
     if ( $lc_parts[0] eq '' )
     {
       $lc_known = 10;
+    }
+    if ( $lc_parts[0] eq 'fi' )
+    {
+      $lc_returno = [@lc_algo];
+      $_[0] = $lc_rcont;
+      return $lc_returno;
     }
     
     if ( $lc_known < 5 )
     {
       die "\nFATAL ERROR:\n"
-        . "  Template file: " . $_[0] . ":\n"
+        . "  Template file: " . $lc_filename . ":\n"
         . "  Unknown Directive: " . $lc_parts[0] . ":\n"
       . "\n";
     }
   }
   
   
-  $this->{'alg'} = [@lc_algo];
-  return $this;
+  $lc_returno = [@lc_algo];
+  $_[0] = $lc_rcont;
+  return $lc_returno;
 }
 
 sub escap {
