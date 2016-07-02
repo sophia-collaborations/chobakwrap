89daccdc7df04e2f2f194bc40c7edc9a0a34685c
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-07-02 17:23:36 -0400

    Began documentation on this own package.

--- inner-wrap.pl
+++ inner-wrap.pl
@@ -27,6 +27,7 @@ sub opto__f_rloc {
 
 &argola::help_opt('--help-sound','manp/chobakwrap-sound.1');
 &argola::help_opt('--help-caff','manp/chobakwrap-caff.1');
+&argola::help_opt('--help','manp/chobakwrap.1');
 
 &argola::txt_spit_opt('--out-install','pub-res/srv/install.sh.txt');
 &argola::txt_spit_opt('--out-gitignore','pub-res/srv/gitignore.txt');
