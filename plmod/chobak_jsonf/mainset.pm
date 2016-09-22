3153a8396445712afb06f262d2fa6d07ee925756
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-09-22 18:24:29 -0400

    Added reference-file local pathname finder

--- plmod/chobak_jsonf/mainset.pm
+++ plmod/chobak_jsonf/mainset.pm
@@ -5,6 +5,7 @@ use strict;
 sub cont {
   my $this;
   $this = shift;
+  
   return($this->{'cont'});
 }
 
@@ -12,10 +13,20 @@ sub save {
   my $this;
   my $lc_fnc;
   $this = shift;
+  
   &chobak_json::savef($this->{'cont'},$this->{'write'});
   $lc_fnc = $this->{'swap'};
   &$lc_fnc($this);
   system('rm','-rf',$this->{'write'});
+}
+
+sub reffile {
+  my $this;
+  my $lc_a;
+  $this = shift;
+  
+  $lc_a = $this->{'base'} . '.ref';
+  return $lc_a;
 }
 
 1;
