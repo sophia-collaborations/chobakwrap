1f8daa2e1bd6e9164ff6e2958fe8130373533edc
Sophia Shapira <Sophia.Shapira@gmail.com>
2017-02-14 17:33:15 -0500

    Added provision to insist the .DS_Store is in the gitignore

--- pub-res/ins-pl/checkbefore.pl
+++ pub-res/ins-pl/checkbefore.pl
@@ -2,6 +2,8 @@ use strict;
 
 my $erras = 0;
 
+my $diviso = "\n";
+
 
 my $appresdir;
 my @gitignores;
@@ -61,6 +63,45 @@ sub gitigsee_k {
   $erras = 2;
   print STDERR 'FATAL: Line Missing from .gitignore: ' . $lc_arg . ":\n";
 }
+sub from_gitis_yet {
+  my $lc_arg;
+  my $lc_line;
+  my $lc_loca;
+  $lc_arg = $_[0];
+  foreach $lc_line (@gitignores)
+  {
+    if ( $lc_line eq $lc_arg ) { return; }
+  }
+  print STDERR $diviso . 'WARNING FUTURE-FATAL: Line Missing from .gitignore: ' . $lc_arg . ":\n";
+  $diviso = '';
+  $lc_loca = `pwd`; chomp($lc_loca);
+  print STDERR "EDIT THIS FILE:\n  " . $lc_loca . "/.gitignore :\n";
+  print STDERR '    Warning to pause for ' . $_[1] . ' seconds in proportion to time since implemented:' . "\n\n";
+  if ( $_[1] > 0.5 ) { sleep($_[1]); }
+}
+
+sub from_gitseek {
+  my $lc_now;
+  my $lc_secs;
+  my $lc_sqa;
+  my @lc_sqb;
+  my $lc_sqc;
+  $lc_now = `date +%s`; chomp($lc_now);
+  if ( $lc_now < $_[0] ) { return; }
+  $lc_secs = int(($lc_now - $_[0]) / $_[1]);
+  
+  $lc_sqa = $_[3];
+  @lc_sqb = @$lc_sqa;
+  
+  if ( $lc_secs > ($_[2] - 0.5) )
+  {
+    return &gitigseek(@lc_sqb);
+  }
+  foreach $lc_sqc (@lc_sqb) { &from_gitis_yet($lc_sqc,$lc_secs); }
+}
+
+# Added February 12, 2017
+&from_gitseek(1487109871, int((60 * 60 * 24 * 3) + 0.2), 60, ['.DS_Store']);
 
 &gitigseek('/tmp','/ins-opt-code','/build');
 &errot;
