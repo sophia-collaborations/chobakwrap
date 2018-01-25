ca86e45c667d365add1072c9d891b2a63b08e18b
Sophia Shapira <Sophia.Shapira@gmail.com>
2018-01-25 17:21:01 -0500

    Now we have a way that non-option arguments can be accepted.

--- plmod/argola.pm
+++ plmod/argola.pm
@@ -8,6 +8,8 @@ my $versono;
 my $resorco;
 my %opthash = {};
 my @optlist = ();
+my $spare_arg_yes = 0; # Has a spare-argument function been defined?
+my $spare_arg_fnc;     # What function processes spare arguments?
 
 @argbuft = @ARGV;
 $versono = &getrg();
@@ -22,6 +24,11 @@ sub getrg {
   $lc_ret = "";
   if ( $lc_a > 0.5 ) { $lc_ret = shift(@argbuft); }
   return $lc_ret;
+}
+
+sub nonoptarg {
+  $spare_arg_fnc = $_[0];
+  $spare_arg_yes = 10;
 }
 
 sub remrg {
@@ -70,12 +77,17 @@ sub runopts {
         $lc_found = 10;
       }
     }
+    $lc_yot = ( 2 > 1 );
     if ( $lc_found < 5 )
     {
-      die "\n" . $_[0] . ": FATAL ERROR:\nUnknown Option: " . $lc_crg . ":\n\n";
+      if ( $spare_arg_yes < 5 )
+      {
+        die "\n" . $_[0] . ": FATAL ERROR:\nUnknown Option: " . $lc_crg . ":\n\n";
+      }
+      &$spare_arg_fnc($lc_crg);
+      $lc_yot = ( 1 > 2 );
     }
     
-    $lc_yot = ( 2 > 1 );
     if ( $lc_yot )
     {
       if ( $lc_typ eq 'a' )
